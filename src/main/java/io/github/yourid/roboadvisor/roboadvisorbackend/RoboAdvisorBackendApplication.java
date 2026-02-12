4ad7ac059a40e3f8f06a3a51adff0adc62a0201b
Seong5381 <tjdcks5381@naver.com>
2026-02-13 01:19:33 +0900

    RoboAdvisorBackendApplication

---
+++ src/main/java/io/github/yourid/roboadvisor/roboadvisorbackend/RoboAdvisorBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.yourid.roboadvisor.roboadvisorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboAdvisorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoboAdvisorBackendApplication.class, args);
+    }
+
+}
