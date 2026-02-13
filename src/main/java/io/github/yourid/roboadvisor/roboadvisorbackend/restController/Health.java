02f620fff00e344a2396d11689b799be4322eaf0
Seongchan <tjdcks5381@naver.com>
2026-02-13 11:43:29 +0900

    RoboAdvisorBackendApplication

--- src/main/java/io/github/yourid/roboadvisor/roboadvisorbackend/restController/Health.java
+++ src/main/java/io/github/yourid/roboadvisor/roboadvisorbackend/restController/Health.java
@@ -1,4 +1,4 @@
-package io.github.yourid.roboadvisor.roboadvisorbackend.test;
+package io.github.yourid.roboadvisor.roboadvisorbackend.restController;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
